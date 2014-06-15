15c91842665d2e79cf9be57206b11a4e61ba31ed
Ali Anari <ali@anari.me>
2014-06-14 17:04:55 -0700

    Reduced nesting on 003

--- solutions/003.pl
+++ solutions/003.pl
@@ -1,9 +1,8 @@
 PRIME: for (my $n = 600851475143, my $i = 2; $i < $n/2; $i++) {
-    if ($n % $i == 0) {
-        my $p = $n/$i;
-        for (my $j = 2; $j < $p/2; $j++) {
-            next PRIME if $p % $j == 0;
-        }
-        print $p and exit;
+    next unless $n % $i == 0;
+    my $p = $n/$i;
+    for (my $j = 2; $j < $p/2; $j++) {
+        next PRIME if $p % $j == 0;
     }
+    print $p and exit;
 }
